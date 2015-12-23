3bc04740eccabbdabf82928e7f7f49ee0ce98da1
tsCoelho <tcoelho91@gmail.com>
2015-12-23 23:32:10 +0000

    commiting

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -4,7 +4,7 @@ console.log(bl.sayHello())
 
 var app = angular.module('app',[]);
 
-app.controller('cwCtrl', function($scope, $rootScope){
+app.controller('cwCtrl', function($scope, $http,$rootScope){
 	
 	$scope.currentSection = 1
 
@@ -12,31 +12,33 @@ app.controller('cwCtrl', function($scope, $rootScope){
 		$scope.currentSection = 2
 	}
 	
+	$scope.printLog = function(a){
+		console.log(a)
+	
+	}
+	
+	
 	$scope.testFunc= function(pat){
 		$scope.selPat = pat
 		$scope.currentSection = 3
 		console.log("Patient name is "+pat.name);
-}  
-			});
-	
-
-app.controller('actsCtrl', function($scope, $rootScope, $http){
+		}  
+		
+		
 	$http({
 		method: 'GET',
-		url: 'http://tiago-virtualbox:9000/act'
+		url: 'http://localhost:9000/act'
 	}).then(function successCallback(response) {
 	
     $scope.act = response.data;
 
-  }, function errorCallback(response) {
+	}, function errorCallback(response) {
      
-  });
-})	
-
-app.controller('insuranceReqCtrl', function($scope, $rootScope, $http){
+	});
+	
 	$http({
 		method: 'GET',
-		url: 'http://tiago-virtualbox:9000/request'
+		url: 'http://localhost:9000/request'
 	}).then(function successCallback(response) {
 	
     $scope.request = response.data;
@@ -45,12 +47,9 @@ app.controller('insuranceReqCtrl', function($scope, $rootScope, $http){
      
   });
   
-})			
-			
-app.controller('patientCtrl', function($scope, $rootScope, $http){
-	$http({
+  $http({
 		method: 'GET',
-		url: 'http://tiago-virtualbox:9000/patient'
+		url: 'http://localhost:9000/patient'
 	}).then(function successCallback(response) {
 		
     $scope.patient = response.data;
@@ -59,4 +58,42 @@ app.controller('patientCtrl', function($scope, $rootScope, $http){
      
   });
   
-})			
+	
+
+			});
+			
+	app.controller('medActCtrl', function($scope,$http){
+		$http({
+		method: 'GET',
+		url: 'http://localhost:9000/act'
+	}).then(function successCallback(response) {
+		
+    $scope.medActList = response.data;
+
+  }, function errorCallback(response) {
+     
+  });
+		
+	})
+	
+
+	
+//~ 
+//~ app.controller('actCtrl', function($scope, $http){
+//~ 
+//~ })	
+//~ 
+//~ app.controller('insuranceReqCtrl', function($scope, $rootScope, $http){
+	//~ 
+  //~ 
+//~ })			
+			//~ 
+//~ app.controller('patientCtrl', function($scope, $rootScope, $http){
+	//~ 
+  //~ 
+//~ })		
+//~ 
+//~ app.controller('newCtrl', function($scope, $rootScope, $http){
+	//~ 
+  //~ 
+//~ })		
